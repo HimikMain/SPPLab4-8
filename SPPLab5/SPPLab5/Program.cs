ca6e8562bc741c0451547f3908d1f305fcac4d97
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-12-07 21:47:10 +0300

    5-ая лаба

---
+++ SPPLab5/SPPLab5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SPPLab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFile(Console.ReadLine());
+
+            List<Type> types = assembly.GetTypes().ToList();
+            types = types.Where(type => type.IsPublic).OrderBy(type => type.Namespace).ThenBy(type => type.Name).ToList();
+            foreach (Type publicTypes in types)
+                Console.WriteLine(publicTypes.ToString());
+            Console.ReadLine();
+        }
+    }
+}
