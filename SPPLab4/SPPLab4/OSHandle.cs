0cbb16a3f85ef26a9e3f5eed794b3a08ff5dab75
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-12-27 15:13:58 +0300

    4-ая лаба

---
+++ SPPLab4/SPPLab4/OSHandle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SPPLab4
+{
+    public class OSHandle : IDisposable
+    {
+        [DllImport("kernel32")]
+        private static extern bool CloseHandle(IntPtr handle);
+        private bool disposed = false;
+
+        public OSHandle(IntPtr handle)
+        {
+            this.handle = handle;
+        }
+
+        ~OSHandle()
+        {
+            Dispose(false);
+        }
+        private IntPtr handle;
+        public IntPtr Handle
+        {
+            get
+            {
+                if (!disposed)
+                    return handle;
+                else
+                    throw new ObjectDisposedException(ToString());
+            }
+            set
+            {
+                if (!disposed)
+                    handle = value;
+                else
+                    throw new ObjectDisposedException(ToString());
+            }
+        }
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+                disposed = true;
+            }
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (handle != IntPtr.Zero)
+                CloseHandle(handle);
+            if (disposing)
+            {
+                //Dispose 
+            }
+            else
+            {
+                //Finalize 
+            }
+        }
+    }
+}
---
+++ SPPLab4/SPPLab4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SPPLab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OSHandle test = new OSHandle(IntPtr.Zero);
+            Console.WriteLine(test.Handle);
+            test.Dispose();
+            Console.WriteLine(test.Handle);
+            Console.ReadLine();
+        }
+    }
+}
