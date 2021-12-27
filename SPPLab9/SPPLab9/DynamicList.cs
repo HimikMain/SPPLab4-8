cb0d713b1df580880cfac73a1ecb35379500f60c
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-12-27 15:22:56 +0300

    9-я лаба

---
+++ SPPLab9/SPPLab9/DynamicList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SPPLab9
+{
+    public class DynamicList<T> : IEnumerable<T>
+    {
+
+        private T[] arrayList { get; set; }
+        public int count { get; set; }
+        private const int size = 10;
+
+
+        public DynamicList()
+        {
+            arrayList = new T[size];
+        }
+
+        public void Add(T element)
+        {
+            Resize();
+            arrayList[count++] = element;
+        }
+
+        public void Remove(T element)
+        {
+            RemoveAt(Array.IndexOf(arrayList, element));
+        }
+
+        public void RemoveAt(int index)
+        {
+            var newContainer = new T[arrayList.Length - 1];
+            Array.Copy(arrayList, newContainer, index);
+            Array.Copy(arrayList, index + 1, newContainer, index, arrayList.Length - index - 1);
+            arrayList = newContainer;
+            count--;
+        }
+
+        public void Clear()
+        {
+            arrayList = Array.Empty<T>();
+            count = 0;
+        }
+
+        private void Resize()
+        {
+            if (count != arrayList.Length)
+            {
+                return;
+            }
+
+            T[] tmpContainer = new T[arrayList.Length + size];
+            Array.Copy(arrayList, tmpContainer, arrayList.Length);
+            arrayList = tmpContainer;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < count; i++)
+            {
+                yield return arrayList[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ SPPLab9/SPPLab9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPPLab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicList<int> list = new DynamicList<int>();
+
+            for (int i = 1; i <= 10; i++) 
+            {
+                list.Add(i);
+            }
+
+            Console.Write("Лист: ");
+            foreach (int element in list)
+            {
+                Console.Write(element + ",");
+            }
+            Console.WriteLine("\nРазмер: " + list.count);
+
+            list.RemoveAt(1);
+            Console.Write("Лист: ");
+            foreach (int element in list)
+            {
+                Console.Write(element + ",");
+            }
+            Console.Write("\nРазмер: ");
+            Console.WriteLine(list.count);
+
+            list.Clear();
+
+            Console.Write("Лист: ");
+            foreach (int element in list)
+            {
+                Console.Write(element + ",");
+            }
+            Console.Write("\nРазмер: " + list.count);
+        }
+    }
+}
