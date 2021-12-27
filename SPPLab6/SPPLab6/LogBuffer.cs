f672f31665849c01032c46a333992d7dcf352aa7
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-12-27 15:20:10 +0300

    6-я лаба

---
+++ SPPLab6/SPPLab6/LogBuffer.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace SPPLab6
+{
+    public class LogBuffer
+    {
+        private List<string> myListToFile;
+        private readonly int limitTime;
+        private readonly int limitNumbOfItems;
+        private static System.Timers.Timer timer;
+        private readonly string path;
+        private Mutex mt;
+
+        public LogBuffer(int limitTime, int limitNumbOfItems)
+        {
+            this.limitTime = limitTime;
+            this.limitNumbOfItems = limitNumbOfItems;
+            path = @"C:\Labs\SPP\SPPLab6\SPPLab6\test.txt";
+            myListToFile = new List<string>();
+            mt = new Mutex();
+            SetTimer();
+        }
+
+        public void Add(string item)
+        {
+            myListToFile.Add(item);
+
+            if (myListToFile.Count >= limitNumbOfItems)
+            {
+                Thread SaveThread = new Thread(SaveToFile);
+                SaveThread.Start();
+            }
+        }
+
+        private void SaveToFile()
+        {
+            mt.WaitOne();
+            if (myListToFile.Count != 0)
+            {
+                File.AppendAllLines(path, myListToFile);
+                myListToFile.Clear();
+            }
+            mt.ReleaseMutex();
+        }
+
+        private void CallTimerSaveToFile(object source, System.Timers.ElapsedEventArgs e)
+        {
+            if (myListToFile.Count != 0)
+            {
+                SaveToFile();
+            }
+        }
+
+        private void SetTimer()
+        {
+            timer = new System.Timers.Timer(limitTime);
+            timer.Elapsed += CallTimerSaveToFile;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+    }
+}
---
+++ SPPLab6/SPPLab6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SPPLab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogBuffer logBuffer = new LogBuffer(3000, 50);
+            for (int i = 0; i < 10000; i++)
+            {
+                logBuffer.Add(i.ToString());
+            }
+        }
+    }
+}
